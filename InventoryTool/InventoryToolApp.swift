31b25138ad64d10bcdbe593f9739709388ae321c
Glen <hlib.miasnychenko@gmail.com>
2026-01-04 15:12:41 -0600

    Initial Commit

---
+++ InventoryTool/InventoryToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryToolApp.swift
+//  InventoryTool
+//
+//  Created by Hlib Myasnychenko on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct InventoryToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InventoryToolTests/InventoryToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryToolTests.swift
+//  InventoryToolTests
+//
+//  Created by Hlib Myasnychenko on 1/4/26.
+//
+
+import Testing
+@testable import InventoryTool
+
+struct InventoryToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
